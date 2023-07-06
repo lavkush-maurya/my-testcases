cfeaeff87cc9692107f28c22a47be55ed1229730
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-07-06 16:35:12 +0530

    my-First-react-testcases-using-jest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Flex, Input, border } from "@chakra-ui/react";
+import { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [listItems, setListItems] = useState([]);
+  const [newItemText, setNewItemText] = useState("");
+  const Bstyle = {
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "7px",
+    margin: "10px",
+  };
+  const handleCounterClick = (value) => {
+    setCounter((counter) => counter + value);
+  };
+
+  const handleNewItemChange = (e) => {
+    setNewItemText(e.target.value);
+  };
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    setListItems([
+      ...listItems,
+      {
+        text: newItemText,
+        id: listItems.length,
+      },
+    ]);
+    setNewItemText("");
+  };
+
+  const handleRemoveItem = (id) => {
+    const newListItems = listItems.filter((item) => item.id !== id);
+    setListItems(newListItems);
+  };
+
+  const listItemComponents = listItems.map((item) => {
+    return (
+      <Flex
+        justifyContent={"space-between"}
+        data-testid={`item${item.id}`}
+        key={item.id}
+      >
+        <div>{item.text}</div>
+        <div>
+          <Button
+            size="lg"
+            colorScheme="teal"
+            variant="outline"
+            data-testid={`remove-item${item.id}`}
+            onClick={() => handleRemoveItem(item.id)}
+          >
+            Remove
+          </Button>
+        </div>
+      </Flex>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Flex justifyContent={"center"} marginTop={"10%"}>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Counter:
+              <span data-testid="counter-value">{counter}</span>
+            </p>
+            <div>
+              <button
+                style={Bstyle}
+                colorScheme="teal"
+                variant="outline"
+                onClick={() => handleCounterClick(1)}
+              >
+                Increment
+              </button>
+              <button
+                style={Bstyle}
+                colorScheme="teal"
+                variant="outline"
+                onClick={() => handleCounterClick(-1)}
+                disabled={counter <= 0}
+              >
+                Decrement
+              </button>
+            </div>
+            <br />
+            <form onSubmit={handleAddItem}>
+              <label htmlFor="newItem">
+                Create List Item
+                <Input
+                  htmlSize={6}
+                  width="auto"
+                  variant="filled"
+                  id="newItem"
+                  value={newItemText}
+                  onChange={handleNewItemChange}
+                />
+              </label>
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                data-testid="add-item"
+                type="submit"
+              >
+                Add Item
+              </Button>
+            </form>
+            <ul>{listItemComponents}</ul>
+          </header>
+        </div>
+      </Flex>
+    </ChakraProvider>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders the initial counter value", () => {
+    const { getByTestId } = render(<App />);
+    const counterValue = getByTestId("counter-value");
+    expect(counterValue.textContent).toBe("0");
+  });
+
+  test("increments the counter when the increment button is clicked", () => {
+    const { getByText, getByTestId } = render(<App />);
+    const incrementButton = getByText("Increment");
+    fireEvent.click(incrementButton);
+    const counterValue = getByTestId("counter-value");
+    expect(counterValue.textContent).toBe("1");
+  });
+
+  test("decrements the counter when the decrement button is clicked", () => {
+    const { getByText, getByTestId } = render(<App />);
+    const decrementButton = getByText("Decrement");
+    fireEvent.click(decrementButton);
+    const counterValue = getByTestId("counter-value");
+    expect(counterValue.textContent).toBe("-1");
+  });
+
+  test("disables the decrement button when the counter is 0", () => {
+    const { getByText } = render(<App />);
+    const decrementButton = getByText("Decrement");
+    expect(decrementButton).toBeDisabled();
+  });
+
+  test("adds a new item to the list when the form is submitted", () => {
+    const { getByLabelText, getByTestId, getByText } = render(<App />);
+    const newItemInput = getByLabelText("Create List Item");
+    const addItemButton = getByTestId("add-item");
+
+    fireEvent.change(newItemInput, { target: { value: "New Item" } });
+    fireEvent.click(addItemButton);
+
+    const listItem = getByText("New Item");
+    expect(listItem).toBeInTheDocument();
+  });
+
+  test("removes an item from the list when the remove button is clicked", () => {
+    const { getByLabelText, getByTestId, queryByText } = render(<App />);
+    const newItemInput = getByLabelText("Create List Item");
+    const addItemButton = getByTestId("add-item");
+
+    fireEvent.change(newItemInput, { target: { value: "Item to remove" } });
+    fireEvent.click(addItemButton);
+
+    const removeButton = getByTestId("remove-item0");
+    fireEvent.click(removeButton);
+
+    const listItem = queryByText("Item to remove");
+    expect(listItem).toBeNull();
+  });
 });
